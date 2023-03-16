af7a70708df273eeac027cc30db7b90f49d4ca53
Wigginswharf <127901447+Wigginswharf@users.noreply.github.com>
2023-03-16 12:42:01 -0500

    Update Footer.svelte

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -126,8 +126,9 @@
 	<!-- PLEASE DO NOT REMOVE THE COPYRIGHT -->
 	<span class="copyright">&copy; 2021 - {year} <a href="https://github.com/nmelhado/league-page">League Page</a></span>
 	<br />
+	Found and customized by Casey
 	<!-- PLEASE DO NOT REMOVE THE BUILT BY -->
-	<span class="creator">Found and customized by Casey, Built and maintained by <a href="http://www.nmelhado.com/">Nicholas Melhado</a><br /></span> 
+	<span class="creator">Built by <a href="http://www.nmelhado.com/">Nicholas Melhado</a><br /></span> 
 	<!-- You can remove the donation link (although any donations to help
 	 maintain and enhance League Page would be greatly appreciated!) -->
 	Love League Page? Please consider <a href="https://www.buymeacoffee.com/nmelhado">donating</a> to support enhancements or just to say thank you!
