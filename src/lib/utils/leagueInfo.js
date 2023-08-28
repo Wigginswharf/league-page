bcbfdfb4109d286bcd1cba8b2746a8abe62f5d4e
Wigginswharf <127901447+Wigginswharf@users.noreply.github.com>
2023-08-28 18:47:35 -0500

    Update leagueInfo.js
    Added Brit

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -268,7 +268,7 @@ export const managers = [
        "bio": null,
        "photo" : null, // square ratio recommended (no larger than 500x500)
        "fantasyStart": null, // (optional) when did the manager start playing fantasy football
-       "favoriteTeam" : "ind", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+       "favoriteTeam" : "ic", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
        "rival": null, 
        "favoritePlayer": null, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
