d10d659792e74fe3e33c2d701564192d3eab97bd
Wigginswharf <127901447+Wigginswharf@users.noreply.github.com>
2023-08-28 22:05:47 -0500

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -154,7 +154,7 @@
     
     <h3 class="subSectionHeading" bind:this={oneOne}>1.1 Positional Breakdown</h3>
     
-    <p>29 Total Players</p>
+    <p>30 Total Players</p>
     
     <p class="underscore">Starters</p>
     <ul>
