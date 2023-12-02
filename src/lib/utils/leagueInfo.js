ebc2ba4f901d33a79081c745c94f1fe5b495c396
Wigginswharf <127901447+Wigginswharf@users.noreply.github.com>
2023-12-02 09:57:01 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,7 +1,7 @@
 /*   STEP 1   */
 export const leagueID = "917964226944344064"; // your league ID
 export const leagueName = "Brandon and 11 Losers Dynasty League"; // your league name
-export const dues = 100; // (optional) used in template constitution page
+export const dues = "30" ; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
