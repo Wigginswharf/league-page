eef9798428139aa5a47794feb5c7cbdeaab435bd
Wigginswharf <127901447+Wigginswharf@users.noreply.github.com>
2025-12-30 14:47:53 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -253,7 +253,7 @@ export const managers = [
        "tookOver" : "2023", // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location" : "St. Louis, MO", // (optional)
        "bio": null,
-       "photo" : null, // square ratio recommended (no larger than 500x500)
+       "photo" : "/managers/Tommy.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": null, // (optional) when did the manager start playing fantasy football
        "favoriteTeam" : "chi", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": 'Dynasty', // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
